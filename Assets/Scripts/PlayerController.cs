acb2f6c0edb75af261cf0426660655b0fab37d9a
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-03-01 18:33:01 +0100

    Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 moveVal;
+
+    // TODO
+    // Walk left & right
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(moveVal * Time.deltaTime * speed);
     }
+
+    private void OnMovement(InputValue value)
+    {
+        moveVal = value.Get<Vector2>();
+    }
+
+
 }
